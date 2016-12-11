3e49d4e88099ae2f016d00b08b35cae22fb850f8
iLeonidze <ileonidze@inbox.ru>
2016-12-11 11:20:01 +0300

    Initial commit

---
+++ src/main/java/Console.java
@@ -0,0 +1,17 @@
+public class Console {
+    String listen(){
+        return null;
+    }
+    void log(String string){
+
+    }
+    void warn(String string){
+
+    }
+    void error(String string){
+
+    }
+    void info(String string){
+
+    }
+}
